f6c86d433de28a572c993d05af629e685827ce0c
vgabriel-adviters <capacitaciones13@adviters.com>
2023-04-14 14:30:44 -0300

    Estructura inicial del proyecto

---
+++ src/main/java/com/adviters/administracionusuarios/AdministracionDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.adviters.administracionusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adviters/administracionusuarios/AdministracionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adviters.administracionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
